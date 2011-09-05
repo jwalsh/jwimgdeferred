46f0c3088ff797f3d4ed53a9df1e2010c466b00f
Jason Walsh <jason.walsh@memorylane.com>
2011-09-05 10:54:26 -0700

    Using PPK's findPos

--- src/jwimgdeferred.js
+++ src/jwimgdeferred.js
@@ -1,67 +1,82 @@
-// Options 
+var jw = jw || {};
 
-var config = {
-	threshold: { // When are we close enough to pull
-		x: 0, // We have to have the image in the viewport
-		y: 0 
-	}
-}
+jw.jwimgdeferred = (function() {
 
-// class DeferredImages: 
-// class CachedDeferredImages: 
+	// Options 
 
-var imgs = document.querySelectorAll('img');
-// When we've undeferred the image we don't need to reprocess
-var deferredImages = imgs;
+	var config = {
+		threshold: { // When are we close enough to pull
+			x: 0, // We have to have the image in the viewport
+			y: 0 
+		}
+	};
 
-function undeferImg(img) { 
-	var deferredAvatarSrc = img.getAttribute('src');
-	// TODO: Check against future implementations
-	var imgSrc = deferredAvatarSrc.split('?')[0]; 
-	console.log('Undeferring ' + deferredAvatarSrc);
-	img.setAttribute('src', imgSrc);
-}
-
-
-function imgInView(img) { 
-	var viewBottomThreshold = window.pageYOffset + window.innerHeight + config.threshold.y;
-	console.log(img.offsetTop + '<' + viewBottomThreshold);
-	if (img.offsetTop <= viewBottomThreshold) {
-		return true;
-	}
-}
-
-function checkDeferredImgs() {
-	for (var i = 0; i < deferredImages.length; i++) { 
-		if(imgInView(deferredImages[i])) { 
-			undeferImg(deferredImages[i]);
+	// Dimensions utility 
+	// Externally sourced from http://www.quirksmode.org/js/findpos.html
+	function findPos(obj) {
+		var _left = _top = 0;
+		if (obj.offsetParent) {
+			do {
+				_left += obj.offsetLeft;
+				_top += obj.offsetTop;
+			} while (obj = obj.offsetParent);
+			return {top: _top, left: _left};
 		}
 	}
-}
 
-checkDeferredImgs(); 
+	// class DeferredImages: 
+	// class CachedDeferredImages: 
 
+	var imgs = document.querySelectorAll('img');
+	// When we've undeferred the image we don't need to reprocess
+	var deferredImages = imgs;
 
-var body = document.querySelector('body');
-
-function logEvent(e) { 
-	if (e.type === 'scroll') { 
-
-	} else { 
-		console.log(e);
+	function undeferImg(img) { 
+		var deferredAvatarSrc = img.getAttribute('src');
+		// TODO: Check against future implementations
+		var imgSrc = deferredAvatarSrc.split('?')[0]; 
+		console.log('Undeferring ' + deferredAvatarSrc);
+		img.setAttribute('src', imgSrc);
 	}
 
-}
 
-// TODO: attachEvent
-window.addEventListener('scroll', logEvent);
-window.addEventListener('load', logEvent);
-window.addEventListener('DOMContentLoaded', logEvent);
-// window.onscroll = logEvent(e);
+	function imgInView(img) { 
+		var viewBottomThreshold = window.pageYOffset + window.innerHeight + config.threshold.y;
+		var imgOffsetTop = findPos(img).top;
+		if (imgOffsetTop <= viewBottomThreshold) {
+			console.log(imgOffsetTop + '<' + viewBottomThreshold);
+			return true;
+		}
+	}
 
-function init() { 
-	console.log('initializing');
-	console.info('window dimensions: ' + window.innerWidth + ',' + window.innerHeight);
-}
+	function checkDeferredImgs() {
+		for (var i = 0; i < deferredImages.length; i++) { 
+			if(imgInView(deferredImages[i])) { 
+				undeferImg(deferredImages[i]);
+			}
+		}
+	}
 
-window.addEventListener('load', init);
+	function logEvent(e) { 
+		if (e.type === 'scroll') { 
+
+		} else { 
+			console.log(e);
+		}
+
+	}
+
+	// TODO: attachEvent
+	window.addEventListener('scroll', logEvent);
+	window.addEventListener('load', logEvent);
+	window.addEventListener('DOMContentLoaded', logEvent);
+
+	function init() { 
+		console.log('initializing');
+		console.info('window dimensions: ' + window.innerWidth + ',' + window.innerHeight);
+		checkDeferredImgs(); 
+	}
+
+	window.addEventListener('load', init);
+
+}());
